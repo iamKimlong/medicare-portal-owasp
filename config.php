e2848f0a50a43ed9b8e39144d9329cf6f56d42b5
iamKimlong <iamKimlong@users.noreply.github.com>
2026-03-27 01:50:46 +0700

    fix: remove db test credential

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ define('SECURE_MODE', false);
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'awdonline');
+define('DB_PASS', '');
 define('DB_NAME', 'medicare');
 
 function getDB(): PDO {
